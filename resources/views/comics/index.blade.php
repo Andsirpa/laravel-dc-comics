917f07e427a1ae3a8362fd88cd113cc96d2ef176
Andsirpa <andsirpa94@gmail.com>
2024-03-31 11:33:29 +0200

    feat: add Modal in index and show

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -6,6 +6,8 @@
     <section>
         <div class="container py-4">
             <h1>Comics List</h1>
+            <a class="btn btn-primary mt-3 mb-4" href="{{ route('comics.create') }}">Create New Comic</a>
+
             <table class="table">
                 <thead>
                     <tr>
@@ -34,15 +36,57 @@
                             <a href="{{ route('comics.show', $comic) }}" class="me-2">
                                 <i class="fa-solid fa-eye"></i>
                             </a>
-                        @empty
-                            <tr>
-                                <td colspan="100%">
-                                    No comics found
-                                </td>
-                            </tr>
+                            {{-- al click del bottone faccio apparire la modal --}}
+                            <button type="button" class="modal-button" data-bs-toggle="modal"
+                                data-bs-target="#delete-comic-{{ $comic->id }}">
+                                <i class="fa-solid fa-circle-xmark" style="color: red;"></i>
+                            </button>
+                        </td>
+                    @empty
+                        <tr>
+                            <td colspan="100%">
+                                No comics found
+                            </td>
+                        </tr>
                     @endforelse
                 </tbody>
             </table>
+            {{ $comics->links() }}
         </div>
     </section>
+@endsection
+
+{{-- modal --}}
+@section('modal')
+    @foreach ($comics as $comic)
+        <div class="modal fade" id="delete-comic-{{ $comic->id }}" tabindex="-1"
+            aria-labelledby="delete-comic-{{ $comic->id }}" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5 text-light" id="exampleModalLabel">Delete {{ $comic->name }}
+                        </h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        This Comic will be eliminated. Are you sure?
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <form action="{{ route('comics.destroy', $comic) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger" value="Delete">Delete</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
+@endsection
+
+@section('css')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 @endsection
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -4,26 +4,46 @@
     <section class="container mt-5">
         <h1 class="text-light text-center">{{ $comic->name }}</h1>
 
-
         <a class="btn btn-primary mt-3 mb-4" href="{{ route('comics.create') }}">Create</a>
         <a class="btn btn-primary mt-3 mb-4" href="{{ route('comics.index') }}">Back to Comics List</a>
 
-        <ul>
-            @if (!is_null($comic->description))
-                <div class="mb-2"><b class="fs-5">Description:</b> {{ $comic->description }}</div>
-            @endif
+        @if (!is_null($comic->description))
+            <div class="mb-2"><b class="fs-5">Description:</b> {{ $comic->description }}</div>
+        @endif
 
-            <div class="mb-2"><b class="fs-5">Title:</b> {{ $comic->title }}</div>
-            <div class="mb-2"><b class="fs-5">Series:</b> {{ $comic->series }}</div>
-            <div class="mb-2"><b class="fs-5">Sale Date:</b> {{ $comic->sale_date }}</div>
-            <div class="mb-2"><b class="fs-5">Type:</b> {{ $comic->type }}</div>
-
-            {{-- <li>Series: {{ $comic->series }}</li>
-            <li>Title: {{ $comic->title }}</li>
-            <li>Sale Date: {{ $comic->sale_date }}</li>
-            <li>Type: {{ $comic->type }}</li> --}}
-        </ul>
-
+        <div class="mb-2"><b class="fs-5">Title:</b> {{ $comic->title }}</div>
+        <div class="mb-2"><b class="fs-5">Series:</b> {{ $comic->series }}</div>
+        <div class="mb-2"><b class="fs-5">Sale Date:</b> {{ $comic->sale_date }}</div>
+        <div class="mb-2"><b class="fs-5">Type:</b> {{ $comic->type }}</div>
 
     </section>
+@endsection
+
+{{-- modal --}}
+@section('modal')
+    @foreach ($comics as $comic)
+        <div class="modal fade" id="delete-comic-{{ $comic->id }}" tabindex="-1"
+            aria-labelledby="delete-comic-{{ $comic->id }}" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5 text-light" id="exampleModalLabel">Delete {{ $comic->name }}
+                        </h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        This Comic will be eliminated. Are you sure?
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <form action="{{ route('comics.destroy', $comic) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger" value="Delete">Delete</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
 @endsection
